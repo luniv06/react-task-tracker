3ae806f7d2f3e7b7cf7dcddb8a828142d2861d12
Vinul James <vinuljames@gmail.com>
2022-01-12 17:17:18 +0530

    State & useState hooks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
-function App() {
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id:1,
+      text: 'Doctors appointment',
+      day: 'Feb 5th at 2:30 pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Meeting at school',
+      day: 'Feb 6th at 1:30 pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Food Shopping',
+      day: 'Feb 7th at 4:30 pm',
+      reminder: true,
+    },
+  ])
+
+  const deleteTask = (id) => {
+    // console.log('delete', id);
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
+
+  const toggleReminder = (id) => {
+    // console.log(id);
+    setTasks(tasks.map((task) => task.id === id ? 
+    {...task, reminder: !task.reminder} : task
+    ))
+  }
+
   return (
     <>
     <div className="container">
-      <h1>hello world XD</h1>
+      <Header title="Task Tracker"/>
+      <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
     </div>
     </>
   );
